1474cc5f658c31662712241f3dcfd933d3a76056
Серебряков Сергей Юрьевич <dertefter@gmail.com>
2026-05-13 16:21:54 +0700

    update version

--- feature_wearable/calendar/build.gradle.kts
+++ feature_wearable/calendar/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 android {
     namespace = "com.dertefter.calendar"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 26
--- feature_wearable/home/build.gradle.kts
+++ feature_wearable/home/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 
 android {
     namespace = "com.dertefter.home"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 26
